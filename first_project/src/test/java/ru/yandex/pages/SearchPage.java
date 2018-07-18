fcf9972b6b8a3f27c4050d8648b619d545d0fe4b
romanmpda <41083817+romanmpda@users.noreply.github.com>
2018-07-18 10:54:48 +0300

    Update SearchPage.java

--- first_project/src/test/java/ru/yandex/pages/SearchPage.java
+++ first_project/src/test/java/ru/yandex/pages/SearchPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class SearchPage {
    public SearchPage(WebDriver driver) {
     PageFactory.initElements(driver, this);
@@ -13,30 +12,21 @@ public class SearchPage {
     public WebDriver driver;
    @FindBy(id = "text")
    private WebElement inputField;
-
     @FindBy(css = "div .search2__button")
    private WebElement searchButton;
-
     @FindBy(css = "div  a[href='https://yandex.ru/pogoda/penza?from=serp_title'] .organic__url-text")
-    private WebElement firstLink ;
-
-    public void inputSearchText(String request)
-    {
+    private WebElement firstLink;
+    public void inputSearchText(String request){
         inputField.sendKeys(request);
     }
-    public void clearInputField()
-    {
+    public void clearInputField(){
         inputField.clear();
     }
-    public void clickSearchButton()
-    {
+    public void clickSearchButton(){
         searchButton.click();
     }
-    public String grtFirstLinkText()
-    {
+    public String grtFirstLinkText(){
         String firstLikString = firstLink.getText();
         return firstLikString;
     }
-
-
 }
