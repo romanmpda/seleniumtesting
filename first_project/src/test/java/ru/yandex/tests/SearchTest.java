b5ab67af416815c38daca5390a18bd531f5d68d3
romanmpda <41083817+romanmpda@users.noreply.github.com>
2018-07-18 10:57:03 +0300

    Update SearchTest.java

--- first_project/src/test/java/ru/yandex/tests/SearchTest.java
+++ first_project/src/test/java/ru/yandex/tests/SearchTest.java
@@ -1,29 +1,14 @@
 package ru.yandex.tests;
-
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-
 public class SearchTest extends WebDriverSettings {
-
-
 @Test
     public void searchTest() {
-
-
         searchPage.clearInputField();
         searchPage.inputSearchText("погода пенза");
         searchPage.clickSearchButton();
         String firstLikString = searchPage.grtFirstLinkText();
         Assert.assertEquals("Погода в Пензе", firstLikString);
-
-
-
-
-
-
     }
-
 }
