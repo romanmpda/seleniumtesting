a9b02c6201d13148db19f2927b63255317f769b5
romanmpda <41083817+romanmpda@users.noreply.github.com>
2018-07-18 14:36:00 +0300

    Update SearchPage.java

--- first_project/src/test/java/ru/yandex/pages/SearchPage.java
+++ first_project/src/test/java/ru/yandex/pages/SearchPage.java
@@ -12,26 +12,27 @@ public class SearchPage {
    
     public WebDriver driver;
    
-   @FindBy(id = "text")
-   private WebElement inputField;
+      @FindBy(id = "text")
+      private WebElement inputField;
+      @FindBy(css = "div .search2__button")
+      private WebElement searchButton;
+      @FindBy(css = "div  a[href='https://yandex.ru/pogoda/penza?from=serp_title'] .organic__url-text")
+      private WebElement firstLink;
+
+      public void inputSearchText(String request){
+           inputField.sendKeys(request);
+       }
    
-    @FindBy(css = "div .search2__button")
-   private WebElement searchButton;
+      public void clearInputField(){
+           inputField.clear();
+       }
    
-    @FindBy(css = "div  a[href='https://yandex.ru/pogoda/penza?from=serp_title'] .organic__url-text")
-    private WebElement firstLink;
+      public void clickSearchButton(){
+           searchButton.click();
+       }
    
-    public void inputSearchText(String request){
-        inputField.sendKeys(request);
-    }
-    public void clearInputField(){
-        inputField.clear();
-    }
-    public void clickSearchButton(){
-        searchButton.click();
-    }
-    public String grtFirstLinkText(){
-        String firstLikString = firstLink.getText();
-        return firstLikString;
+      public String grtFirstLinkText(){
+           String firstLikString = firstLink.getText();
+           return firstLikString;
     }
 }
